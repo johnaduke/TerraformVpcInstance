30bb63edafd2700b1fb9bce5367e9c30f247a8dd
jduke-at-631648984499 <jduke@johnduke.co>
2021-08-08 21:02:05 -0400

    hardocded subnet

--- main.tf
+++ main.tf
@@ -12,6 +12,6 @@ module "application-instance" {
   source  = "app.terraform.io/Johns-Org/application-instance/aws"
   version = "0.0.8"
   public_ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAjPOZ+5g+VYMO4+8tFnCzm33Y+VYOaDcMoTbYWRgaNT94cQ9cYnyahsxSIwkcL2SJfhlwe1X2ULhEc5gHwvATilf7zYKfWddQ5WMwjmdVViqPG0CVc+yZnFAHQAqTBy0lV1xGPYyeFFZRNyXqu899nit/pB1WxY0PjzR3parwkG4U1VA4xOjx2ChfQOKnecZp6oETVN/diVrL9tCGavHih9eTfZkylnCL2DriHDzBMqn1+JleRKPsaPDNQ7+dxjjEbdAC2zLIp+iYZ4reh6cok1j8RE8LA8g5RlcceI4FLz7ELEYn0zeyQP/af58arj87+Ihp7dGQDygy6/0SdifVSw== rsa-key-20210808"
-  public_subnet_id = "${module.vpc.public_subnets}"
+  public_subnet_id = "subnet-08e1f5b46045cc775"
   vpc_id = "${module.vpc.vpc_id}"
 }
